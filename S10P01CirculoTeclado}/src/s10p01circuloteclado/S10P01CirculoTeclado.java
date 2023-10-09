5b3e4823bb41219c7ce9866af7a7c82180444b25
Santiago de la cruz Martinez Lara <177685@upslp.edu.mx>
2023-10-09 08:49:37 -0600

    S10P01 - S10P02

--- S10P01CirculoTeclado}/src/s10p01circuloteclado/S10P01CirculoTeclado.java
+++ S10P01CirculoTeclado}/src/s10p01circuloteclado/S10P01CirculoTeclado.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package s10p01circuloteclado;
+
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -10,7 +12,11 @@ import javax.swing.*;
  *
  * @author 177685
  */
-public class S10P01CirculoTeclado {
+public class S10P01CirculoTeclado  extends JFrame implements KeyListener {
+
+    private int x = 200, y = 200;
+    private int ancho = 50, alto = 50;
+    JPanel panel;
 
     /**
      * @param args the command line arguments
@@ -19,4 +25,25 @@ public class S10P01CirculoTeclado {
         // TODO code application logic here
     }
     
+    private void crearGUI(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        Graphics papel = panel.getGraphics();
+        papel.setColor(Color.WHITE);
+        papel.fillReact(0,0,700,700);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
